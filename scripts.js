4a8200dec9821820f39c13a8010a0a092f41cc46
David Becker <david@beckersite.com>
2017-05-15 16:57:04 -0600

    Create constructor function for bookmarks

--- scripts.js
+++ scripts.js
@@ -1,6 +1,16 @@
-// var $webTitle = $('#website-title');
-// var $webURL = $('#website-url');
-// var $enterBtn = $('.enter-btn');
-// var $readBtn = $('.read-btn');
-// var $deleteBtn = $('.delete-btn');
-// var $bookmarkLibrary = $('.bookmark-container');
+var $title = $('#website-title').val();
+var $url = $('#website-url').val();
+var $enterBtn = $('.enter-btn');
+var $readBtn = $('.read-btn');
+var $deleteBtn = $('.delete-btn');
+var $bookmarkLibrary = $('.bookmark-container');
+
+
+
+//Constructor function in prepend()
+function Bookmark(title, url) {
+  this.title = title;
+  this.url = url;
+}
+
+var bookmark = new Bookmark($title, $url);
