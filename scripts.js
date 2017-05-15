62d1dd98cb14fff2ac67a89d656c0b46e0e4a04d
David Becker <david@beckersite.com>
2017-05-15 17:17:11 -0600

    Create prepend function for inputs

--- scripts.js
+++ scripts.js
@@ -1,5 +1,3 @@
-var $title = $('#website-title').val();
-var $url = $('#website-url').val();
 var $enterBtn = $('.enter-btn');
 var $readBtn = $('.read-btn');
 var $deleteBtn = $('.delete-btn');
@@ -7,10 +5,34 @@ var $bookmarkLibrary = $('.bookmark-container');
 
 
 
-//Constructor function in prepend()
-function Bookmark(title, url) {
-  this.title = title;
-  this.url = url;
-}
+$enterBtn.on('click', function() {
+  prependBookmark();
+  console.log('button working')
+})
 
-var bookmark = new Bookmark($title, $url);
+//Constructor function in prepend() if needed
+// function Bookmark(title, url) {
+//   this.title = title;
+//   this.url = url;
+// }
+// var bookmark = new Bookmark($title, $url);
+
+
+function prependBookmark() {
+
+  var $title = $('#website-title').val();
+  var $url = $('#website-url').val();
+
+  $bookmarkLibrary.prepend(
+    `<article class="bookmark read">
+      <div class="content">
+        <h2 class="title">${$title}</h2>
+        <a class="link" href="${$url}">${$url}</a>
+        <div class="btn-container">
+          <button class="read-btn" type="button" name="read">Read</button>
+          <button class="delete-btn" type="button" name="delete">Delete</button>
+        </div>
+      </div>
+    </article>`
+  )
+}
