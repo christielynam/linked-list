600b7e1628ff86f4a24fb9ec61f501c04f1fefa8
Christie Lynam <christielynam@gmail.com>
2017-05-16 14:37:13 -0600

    clean up JS

--- scripts.js
+++ scripts.js
@@ -1,47 +1,48 @@
-$(window).on('load', function() {
-  $('#website-title').focus();
-})
-
-
-
-
-var $enterBtn = $('.enter-btn');
-var $deleteBtn = $('.delete-btn');
 var totalCounter = 0;
 var readCounter = 0;
 var unreadCounter = 0;
 
+$(window).on('load', function() {
+  $('#website-title').focus();
+})
 
-function countBookmarks(){
-  totalCounter = readCounter + unreadCounter;
-  $('.total-count').text(totalCounter);
-  $('.read-count').text(readCounter);
-  $('.unread-count').text(unreadCounter);
-}
+$('#website-title').on('input', function() {
+  toggleEnterDisable();
+})
 
+$('#website-url').on('input', function() {
+  toggleEnterDisable();
+})
 
-$enterBtn.on('click', function() {
+$('.enter-btn').on('click', function() {
   prependBookmark();
   clearInput();
   toggleEnterDisable();
   unreadCounter++
   countBookmarks();
-  })
+})
 
-//Constructor function in prepend() if needed
-// function Bookmark(title, url) {
-//   this.title = title;
-//   this.url = url;
-// }
-// var bookmark = new Bookmark($title, $url);
+$('.bookmark-container').on('click', '.read-btn', function() {
+  $('article').toggleClass('read');
+  console.log('pressing');
+})
+
+$('.bookmark-container').on('click', '.delete-btn', function() {
+  $(this).parents('.bookmark').remove();
+  $('#website-title').focus();
+
+  if(readCounter > 0) {
+    readCounter = readCounter - 1;
+  }
+  unreadCounter = unreadCounter - 1;
+  countBookmarks();
+});
 
 
 function prependBookmark() {
-
   var $bookmarkLibrary = $('.bookmark-container');
   var $title = $('#website-title').val();
   var $url = $('#website-url').val();
-
   $bookmarkLibrary.prepend(
     `<article class="bookmark">
       <div class="content">
@@ -56,31 +57,16 @@ function prependBookmark() {
   )
 }
 
-$('.bookmark-container').on('click', '.read-btn', function() {
-  $('article').toggleClass('read');
-  console.log('pressing');
-
-
-})
-
 function toggleEnterDisable() {
   var $title = $('#website-title').val();
   var $url = $('#website-url').val();
 
   if (($title.length === 0) || ($url.length === 0)) {
-    $enterBtn.prop('disabled', true);
+    $('.enter-btn').prop('disabled', true);
   } else {
-    $enterBtn.prop('disabled', false);
+    $('.enter-btn').prop('disabled', false);
   }
 }
-
-
-$('#website-title').on('input', function() {
-  toggleEnterDisable();
-})
-$('#website-url').on('input', function() {
-  toggleEnterDisable();
-})
 
 function clearInput() {
   $('#website-title').val('');
@@ -88,13 +74,9 @@ function clearInput() {
   $('#website-title').focus();
 }
 
-$('.bookmark-container').on('click', '.delete-btn', function() {
-  $(this).parents('.bookmark').remove();
-  $('#website-title').focus();
-
-  if(readCounter > 0) {
-    readCounter = readCounter - 1;
-  }
-  unreadCounter = unreadCounter - 1;
-  countBookmarks();
-});
+function countBookmarks(){
+  totalCounter = readCounter + unreadCounter;
+  $('.total-count').text(totalCounter);
+  $('.read-count').text(readCounter);
+  $('.unread-count').text(unreadCounter);
+}
